41fb3c11755b00617aaca71d3f778780b4bf8aae
UnaiManzanoPerez <79275029+UnaiManzanoPerez@users.noreply.github.com>
2022-02-15 20:46:03 +0100

    angular implementado con bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartaCastellanoComponent } from './carta-castellano/carta-castellano.component';
+import { CartaCatalaComponent } from './carta-catala/carta-catala.component';
+import { InformacionComponent } from './informacion/informacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"catalan",component:CartaCatalaComponent},
+  {path:"castellano",component:CartaCastellanoComponent},
+  {path:"informacion",component:InformacionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartaCatalaComponent } from './carta-catala/carta-catala.component';
+import { CartaCastellanoComponent } from './carta-castellano/carta-castellano.component';
+import { InformacionComponent } from './informacion/informacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartaCatalaComponent,
+    CartaCastellanoComponent,
+    InformacionComponent
   ],
   imports: [
     BrowserModule,
